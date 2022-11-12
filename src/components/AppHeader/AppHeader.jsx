36e7292ad1dcfbb5dbb48f6deaf4e5bf89119669
Ivan Ershov <Navi133@yandex.ru>
2022-11-13 02:30:32 +0300

    night update AppHeader

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -12,19 +12,19 @@ function AppHeader() {
     <header className={`${styles.header} mt-10 mb-10`}>
       <nav className={`${styles.nav} pt-4 pb-4`}>
         <a href="#" className={`${styles.link} pt-4 pl-5 pb-4 pr-5 mr-2`}>
-          <BurgerIcon className="ml-2" type="primary" />
-          <span className="ml-8">Конструткор</span>
+          <BurgerIcon type="primary" />
+          <span className={`${styles.link_active} ml-2`}>Конструктор</span>
         </a>
         <a href="#" className={`${styles.link} pt-4 pl-5 pb-4 pr-5 mr-2`}>
           <ListIcon type="primary" />
-          <span className="ml-8">Лента заказов</span>
+          <span className={`${styles.link_text} ml-2`}>Лента заказов</span>
         </a>
         <a href="#" className={styles.logo}>
           <Logo />
         </a>
         <a href="#" className={`${styles.link} pt-4 pl-5 pb-4 pr-5 mr-2`}>
           <ProfileIcon type="primary" />
-          <span className="ml-8">Личный кабинет</span>
+          <span className={`${styles.link_text} ml-2`}>Личный кабинет</span>
         </a>
       </nav>
     </header>
