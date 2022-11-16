5d00e090e6283eb2aada508ab1747dac8adb3751
Ivan Ershov <Navi133@yandex.ru>
2022-11-16 22:33:52 +0300

    1 step done

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -5,17 +5,16 @@ import styles from "./BurgerConstructor.module.css";
 import { data } from '../utils/data.js'
 
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const songsObject = JSON.stringify(data);
 const dataObj = JSON.parse(songsObject);
 
-console.log(dataObj);
-
 function BurgerConstructor() {
   return (
-    <section className={`${styles.section} pt-25 pl-4 pr-4`}>
+    <section className={`${styles.section} pt-25 pl-4`}>
       <div className="pl-8 mb-4">
         <ConstructorElement 
           className="ml-8"
@@ -26,73 +25,82 @@ function BurgerConstructor() {
           thumbnail={dataObj[0].image_mobile}
         />
       </div>
-      <div className="pl-8 mb-4">
+      <ul className={`${styles.list} mr-10`}>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[3].name}
+            price={dataObj[3].price}
+            thumbnail={dataObj[3].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[2].name}
+            price={dataObj[2].price}
+            thumbnail={dataObj[2].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[7].name}
+            price={dataObj[7].price}
+            thumbnail={dataObj[7].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[8].name}
+            price={dataObj[8].price}
+            thumbnail={dataObj[8].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[9].name}
+            price={dataObj[9].price}
+            thumbnail={dataObj[9].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[8].name}
+            price={dataObj[8].price}
+            thumbnail={dataObj[8].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[3].name}
+            price={dataObj[3].price}
+            thumbnail={dataObj[3].image_mobile}
+          />
+        </li>
+        <li className={`${styles.item} mb-4 mr-2`}>
+          <DragIcon type="primary" />
+          <ConstructorElement 
+            isLocked={false}
+            text={dataObj[9].name}
+            price={dataObj[9].price}
+            thumbnail={dataObj[9].image_mobile}
+          />
+        </li>
+      </ul>
+      <div className="pl-8 mt-4 mb-10">
         <ConstructorElement 
-          isLocked={false}
-          text={dataObj[3].name}
-          price={dataObj[3].price}
-          thumbnail={dataObj[3].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[2].name}
-          price={dataObj[2].price}
-          thumbnail={dataObj[2].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[7].name}
-          price={dataObj[7].price}
-          thumbnail={dataObj[7].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[8].name}
-          price={dataObj[8].price}
-          thumbnail={dataObj[8].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[9].name}
-          price={dataObj[9].price}
-          thumbnail={dataObj[9].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[8].name}
-          price={dataObj[8].price}
-          thumbnail={dataObj[8].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[3].name}
-          price={dataObj[3].price}
-          thumbnail={dataObj[3].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-4">
-        <ConstructorElement 
-          isLocked={false}
-          text={dataObj[9].name}
-          price={dataObj[9].price}
-          thumbnail={dataObj[9].image_mobile}
-        />
-      </div>
-      <div className="pl-8 mb-10">
-        <ConstructorElement 
-          className="ml-8"
           type="bottom"
           isLocked={true}
           text={`${dataObj[0].name} (верх)`}
@@ -100,7 +108,7 @@ function BurgerConstructor() {
           thumbnail={dataObj[0].image_mobile}
         />
       </div>
-      <div className={`${styles.info} pr-4`}>
+      <div className={styles.info}>
         <p className="text text_type_digits-medium mr-1">6660</p>
         <CurrencyIcon type="primary" />
         <Button htmlType="button" type="primary" size="large" extraClass="ml-10">
